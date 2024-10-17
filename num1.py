e3de1b69b6b6bd51c165ee6cab34edeee994d037
Kembabazi <kembabaziritah0@gmail.com>
2024-10-17 15:27:17 +0300

    Add files via upload

---
+++ num1.py
@@ -0,0 +1,11 @@
+#introduction to python & understanding variables
+#for three years with six semesters
+semester1= 1500000
+semester2= semester1 + 1500000
+semester3 = semester2+ 1500000
+semester4 = semester3 + 1500000
+semester5 = semester4 + 1500000
+semester6 =semester5 +1500000
+print(f"total fees for three years is  {semester6} Ugandan shillings")
+
+
